d8c3bd6e989e3829b87214ceaecf04e329161532
choe18 <rlorlrol562@gmail.com>
2026-05-27 18:29:37 +0900

    fix: 위시리스트 구매 요청 DTO JSON 매핑 수정

--- src/main/java/com/miniproject/be/domain/wishlist/dto/request/WishlistPurchaseRequest.java
+++ src/main/java/com/miniproject/be/domain/wishlist/dto/request/WishlistPurchaseRequest.java
@@ -1,10 +1,12 @@
 package com.miniproject.be.domain.wishlist.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
 public class WishlistPurchaseRequest {
 
-    private boolean isPurchased;
+    @JsonProperty("purchased")
+    private boolean purchased;
 
 }
