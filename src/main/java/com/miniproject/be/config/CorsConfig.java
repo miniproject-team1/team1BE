09db17827e575ac4bdc140626dd23bc7eaea11ea
choe18 <rlorlrol562@gmail.com>
2026-05-31 16:48:00 +0900

    feat: CORS에 프론트 배포 주소 추가

--- src/main/java/com/miniproject/be/config/CorsConfig.java
+++ src/main/java/com/miniproject/be/config/CorsConfig.java
@@ -13,7 +13,8 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins(
                         "http://localhost:3000",
-                        "http://localhost:5173"
+                        "http://localhost:5173",
+                        "https://team1-fe-psi.vercel.app"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
